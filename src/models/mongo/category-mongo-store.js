0fd24bec482ff50b82182d01020fcaf459422359
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2023-03-17 12:06:58 +0000

    Added Category model

---
+++ src/models/mongo/category-mongo-store.js
@@ -0,0 +1,34 @@
+import { Category } from "./category.js";
+
+export const categoryMongoStore = {
+  async getAllCategories() {
+    const categories = await Category.find().lean();
+    return categories;
+  },
+
+  async getCategoryById(id) {
+    if (id) {
+      const category = await Category.findOne({ _id: id }).lean();
+      return category;
+    }
+    return null;
+  },
+
+  async addCategory(category) {
+    const newCategory = new Category(category);
+    const categoryObj = await newCategory.save();
+    return this.getCategroyById(categoryObj._id);
+  },
+
+  async deleteCategoryById(id) {
+    try {
+      await Category.deleteOne({ _id: id });
+    } catch (error) {
+      console.log("bad id");
+    }
+  },
+
+  async deleteAllCategories() {
+    await Categories.deleteMany({});
+  }
+};
