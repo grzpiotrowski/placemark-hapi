4fa4aa7bccc96927a2a0f02922ec212eaf1ad770
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2023-05-24 03:42:37 +0100

    Password hashing and salting

--- src/models/mongo/seed-data.js
+++ src/models/mongo/seed-data.js
@@ -5,21 +5,21 @@ export const seedData = {
         firstName: "Homer",
         lastName: "Simpson",
         email: "homer@simpson.com",
-        password: "secret",
+        password: "$2a$10$HnapEWWKQQ0.0Ft33nXxtuJ0fsmd9ywYswEzT4C.6Ul5y6cOU.UEa",
         scope: ["user", "admin"]
       },
       marge: {
         firstName: "Marge",
         lastName: "Simpson",
         email: "marge@simpson.com",
-        password: "secret",
+        password: "$2a$10$D3TWK8MSAh63Lhk4gIFSJ.ekM368J88qJRUYSbuBfzffex6B0dFwK",
         scope: ["user"]
       },
       bart: {
         firstName: "Bart",
         lastName: "Simpson",
         email: "bart@simpson.com",
-        password: "secret",
+        password: "$2a$10$uqnPlgyVjBEOtcgMhn/WRO3FwW4T9gX36tTB4RqpGeB/LkCR3YQuO",
         scope: ["user"]
       }
     },
